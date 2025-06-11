60708a79d82db8c0e6f11b02c3ad86555d72c207
26nandtanu <150471303+26nandtanu@users.noreply.github.com>
2025-06-11 10:49:25 -0400

    Updates to GamePanel & KeyHandler

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.awt.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 /*♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪
@@ -11,7 +13,6 @@ import javax.swing.*;
   Author: Tanushka Nandanapalli
 
 ♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪*/
-
 public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
@@ -24,9 +25,12 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    // FPS
+    int FPS = 60;
+
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    
+
     // Set player's default position
     int playerX = 100;
     int playerY = 100;
@@ -42,56 +46,68 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void startGameThread() {
-        
+
         gameThread = new Thread(this);
         gameThread.start();
     }
 
     @Override
     public void run() {
-        
+
+        double drawInterval = 1000000000 / FPS; // 0.016667 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
         while (gameThread != null) {
-            
-//            System.out.println("The game loop is running");
-            
+
             // 1 UPDATE: update information such as character positions
             update();
-            
+
             // 2 DRAW: draw the screen with the updated information
             repaint();
-            
-            
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+                
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+                
+                nextDrawTime += drawInterval;
+                
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        
+
     }
-    
+
     public void update() {
-        
+
         if (keyH.upPressed) {
             playerY -= playerSpeed;
-        }
-        else if (keyH.downPressed) {
+        } else if (keyH.downPressed) {
             playerY += playerSpeed;
-        } 
-        else if (keyH.leftPressed) {
+        } else if (keyH.leftPressed) {
             playerX -= playerSpeed;
-        } 
-        else if (keyH.rightPressed) {
+        } else if (keyH.rightPressed) {
             playerX += playerSpeed;
         }
-        
+
     }
-    
+
     public void paintComponent(Graphics g) {
-        
+
         super.paintComponent(g);
-        
+
         Graphics2D g2 = (Graphics2D) g;
-        
+
         g2.setColor(Color.white);
-        
+
         g2.fillRect(playerX, playerY, tileSize, tileSize);
-        
+
         g2.dispose();
     }
 
