ca22ff3ec1f4c8d2c1b53b8c576227c4c3f28868
26nandtanu <150471303+26nandtanu@users.noreply.github.com>
2025-06-12 00:26:38 -0400

    Figured out collision control, so everything moves right (amazing)

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -30,6 +30,12 @@ public class Player extends Entity {
         
         screenX = (gp.screenWidth/2) - (gp.tileSize/2);
         screenY = (gp.screenHeight/2) - (gp.tileSize/2);
+        
+        solidArea = new Rectangle();
+        solidArea.x = 8;
+        solidArea.y = 16;
+        solidArea.width = 32;
+        solidArea.height = 32;
 
         setDefaultValues();
         getPlayerImage();
@@ -73,16 +79,35 @@ public class Player extends Entity {
             
             if (keyH.upPressed) {
                 direction = "up";
-                worldY -= speed;
             } else if (keyH.downPressed) {
                 direction = "down";
-                worldY += speed;
             } else if (keyH.leftPressed) {
                 direction = "left";
-                worldX -= speed;
             } else if (keyH.rightPressed) {
                 direction = "right";
-                worldX += speed;
+            }
+            
+            collisionOn = false;
+            gp.cChecker.checkTile(this);
+            
+            // IF COLLISION IS FALSE, PLAYER CAN MOVE
+            if (!collisionOn) {
+                
+                switch (direction) {
+                    case "up":
+                        worldY -= speed;
+                        break;
+                    case "down":
+                        worldY += speed;
+                        break;
+                    case "left":
+                        worldX -= speed;
+                        break;
+                    case "right":
+                        worldX += speed;
+                        break;
+                }
+                
             }
 
             spriteCounter++;
