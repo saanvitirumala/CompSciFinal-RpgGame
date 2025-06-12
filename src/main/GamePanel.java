2d0ce55d5f26151b2772453f1e867b06e19a1bff
26nandtanu <150471303+26nandtanu@users.noreply.github.com>
2025-06-11 23:54:56 -0400

    A bunch of Stuff (looks amazing though)

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,9 +1,8 @@
 package main;
 
+import tile.TileManager;
 import entity.Player;
 import java.awt.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.*;
 
 /*♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪
@@ -21,22 +20,29 @@ public class GamePanel extends JPanel implements Runnable {
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48x48 tile
-    final int maxScreenCol = 16; // 4:3 ratio
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+    public final int maxScreenCol = 16; // 4:3 ratio
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+    
+    // WORLD SETTINGS   
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
 
     // FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this, keyH);
+    public Player player = new Player(this, keyH);
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.white);
+        this.setBackground(Color.black);
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
@@ -125,6 +131,8 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
+        
+        tileM.draw(g2);
         
         player.draw(g2);
 
--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -1,26 +1,19 @@
-package entity;
+package tile;
 
 import java.awt.image.BufferedImage;
 
-
 /*♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪
 
-    File: Entity.java
+    File: Tile.java
     Date: Jun 11, 2025
  Purpose: 
   Author: Tanushka Nandanapalli
 
 ♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪❤❤❤♪♪♪*/
 
-public class Entity {
+public class Tile {
 
-    public int worldX, worldY;
-    public int speed;
-    
-    public BufferedImage front, up, down, left, right;
-    public String direction;
-    
-    public int spriteCounter = 0;
-    public int spriteNum = 1;
+    public BufferedImage image;
+    public boolean collision = false;
 
 }
