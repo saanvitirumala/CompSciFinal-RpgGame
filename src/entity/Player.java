348cdae64f171955e88eaf1963abc0391fdb3d75
26nandtanu <150471303+26nandtanu@users.noreply.github.com>
2025-06-12 23:21:51 -0400

    updates

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -22,6 +22,7 @@ public class Player extends Entity {
     
     public final int screenX;
     public final int screenY;
+    int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH) {
 
@@ -95,6 +96,7 @@ public class Player extends Entity {
             
             // CHECK OBJECT COLLISION
             int objIndex = gp.cChecker.checkObject(this, true);
+            pickUpObject(objIndex);
             
             // IF COLLISION IS FALSE, PLAYER CAN MOVE
             if (!collisionOn) {
@@ -128,6 +130,31 @@ public class Player extends Entity {
             
         }
 
+    }
+    
+    public void pickUpObject(int i) {
+        
+        if (i != 999) {
+            
+            String objectName = gp.obj[i].name;
+            
+            switch (objectName) {
+                case "Key":
+                    hasKey++;
+                    gp.obj[i] = null;
+                    System.out.println("Key: " + hasKey);
+                    break;
+                case "Door":
+                    if (hasKey > 0) {
+                        gp.obj[i] = null;
+                        hasKey--;
+                    }
+                    System.out.println("Key: " + hasKey);
+                    break;     
+            }
+            
+        }
+        
     }
 
     public void draw(Graphics2D g2) {
--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -90,25 +90,45 @@ public class CollisionChecker {
                     case "up":
                         entity.solidArea.y -= entity.speed;
                         if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-                            System.out.println("Up collision!");
+                            if (gp.obj[i].collision) {
+                                entity.collisionOn = true;
+                            }
+                            if (player) {
+                                index = i;
+                            }
                         }
                         break;
                     case "down":
                         entity.solidArea.y += entity.speed;
                         if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-                            System.out.println("Down collision!");
+                            if (gp.obj[i].collision) {
+                                entity.collisionOn = true;
+                            }
+                            if (player) {
+                                index = i;
+                            }
                         }
                         break;
                     case "left":
                         entity.solidArea.x -= entity.speed;
                         if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-                            System.out.println("Left collision!");
+                            if (gp.obj[i].collision) {
+                                entity.collisionOn = true;
+                            }
+                            if (player) {
+                                index = i;
+                            }
                         }
                         break;
                     case "right":
                         entity.solidArea.x += entity.speed;
                         if (entity.solidArea.intersects(gp.obj[i].solidArea)) {
-                            System.out.println("Right collision!");
+                            if (gp.obj[i].collision) {
+                                entity.collisionOn = true;
+                            }
+                            if (player) {
+                                index = i;
+                            }
                         }
                         break;
                 }
